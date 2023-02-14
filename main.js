797ffbee7f36534b3d2ad8cd5a322f1c6ade387b
Kevin Trabajo <kevin.alvarenga93@gmail.com>
2023-02-14 15:56:37 -0600

    navbar trabajando en conjunto

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
+//variables
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+//funcionalidad toggle 
+
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobilepMenu);
+
+//funciones
 function toggleDesktopMenu() {
-  console.log('Click');
   desktopMenu.classList.toggle('inactive');
+}
+function toggleMobilepMenu () {
+    console.log('click')
+    mobileMenu.classList.toggle('inactive');
 }
