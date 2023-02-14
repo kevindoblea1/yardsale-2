0a5715b6ba114ac1fa3658e7301ba3aaf83c7cb7
Kevin Trabajo <kevin.alvarenga93@gmail.com>
2023-02-14 16:42:08 -0600

    finalizado de aside

--- main.js
+++ main.js
@@ -2,18 +2,27 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 
+const menuCarIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
+
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
-//funcionalidad toggle 
 
+
+//eventos
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobilepMenu);
+menuCarIcon.addEventListener('click', toggleCarAside);
 
 //funciones
 function toggleDesktopMenu() {
-  desktopMenu.classList.toggle('inactive');
+    aside.classList.add('inactive');
+    desktopMenu.classList.toggle('inactive');
 }
 function toggleMobilepMenu () {
-    console.log('click')
     mobileMenu.classList.toggle('inactive');
+}
+function toggleCarAside () {
+    desktopMenu.classList.add('inactive');
+    aside.classList.toggle('inactive');
 }
