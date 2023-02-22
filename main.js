3aee2b842a3359e64a862be3b0d4bbd58128714f
Kevin Trabajo <kevin.alvarenga93@gmail.com>
2023-02-22 16:02:36 -0600

    actualizacion previo a continuar

--- main.js
+++ main.js
@@ -7,7 +7,6 @@ const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const cardsContainer = document.querySelector('.cards-container');
 
-
 //eventos
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobilepMenu);
@@ -80,24 +79,6 @@ productList.push({
   price: 5500,
   image: 'https://images.pexels.com/photos/1119796/pexels-photo-1119796.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
 });
-
-/*
-<div class="product-card">
-<img
-  src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-  alt="">
-<div class="product-info">
-  <div>
-    <p>$120,00</p>
-    <p>Bike</p>
-  </div>
-  <figure>
-    <img src="./icons/bt_add_to_cart.svg" alt="">
-  </figure>
-</div>
-</div>
-*/
-
 function renderProducts(arr) {
   for (product of arr) {
     /*captura de componenetes */
