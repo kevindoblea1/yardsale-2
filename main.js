ac043a4e3994da807495dc37c59fb99bf80eca01
kevindoblea1 <kevin.alvarenga93@gmail.com>
2023-02-25 11:01:04 -0600

    integracion product detail info y menu mobile

--- main.js
+++ main.js
@@ -1,115 +1,154 @@
-//variables
 const menuEmail = document.querySelector('.navbar-email');
-const desktopMenu = document.querySelector('.desktop-menu');
-const menuCarIcon = document.querySelector('.navbar-shopping-cart');
-const aside = document.querySelector('.product-detail');
 const menuHamIcon = document.querySelector('.menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const productDetailCloseIcon = document.querySelector('.product-detail-close')
+const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
+const productDetailContainer = document.querySelector('#productDetail');
 const cardsContainer = document.querySelector('.cards-container');
 
-//eventos
 menuEmail.addEventListener('click', toggleDesktopMenu);
-menuHamIcon.addEventListener('click', toggleMobilepMenu);
-menuCarIcon.addEventListener('click', toggleCarAside);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
+productDetailCloseIcon.addEventListener('click', closeProductDetailAside);
 
-//funciones
 function toggleDesktopMenu() {
-    aside.classList.add('inactive');
-    desktopMenu.classList.toggle('inactive');
-}
-function toggleMobilepMenu () {
-    mobileMenu.classList.toggle('inactive');
-}
-function toggleCarAside () {
-    desktopMenu.classList.add('inactive');
-    aside.classList.toggle('inactive');
+  const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
+
+  if (!isAsideClosed) {
+    shoppingCartContainer.classList.add('inactive');
+  }
+
+  desktopMenu.classList.toggle('inactive');
 }
 
-/*equivalente del array que daria el api rest */
+function toggleMobileMenu() {
+  const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
+
+  if (!isAsideClosed) {
+    shoppingCartContainer.classList.add('inactive');
+  }
+
+  closeProductDetailAside();
+
+  mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCarritoAside() {
+  const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+
+  if (!isMobileMenuClosed) {
+    mobileMenu.classList.add('inactive');
+  }
+
+  const isProductDetailClosed = productDetailContainer.classList.contains('inactive');
+
+  if (!isProductDetailClosed) {
+    productDetailContainer.classList.add('inactive');
+  }
+
+  shoppingCartContainer.classList.toggle('inactive');
+}
+
+function openProductDetailAside() {
+  shoppingCartContainer.classList.add('inactive');
+  productDetailContainer.classList.remove('inactive');
+}
+
+function closeProductDetailAside() {
+  productDetailContainer.classList.add('inactive');
+}
+
 const productList = [];
+productList.push({
+  name: 'Bike',
+  price: 120,
+  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productList.push({
+  name: 'Scoter',
+  price: 4200,
+  image: 'https://images.pexels.com/photos/159192/vespa-roller-motor-scooter-cult-159192.jpeg?auto=compress&cs=tinysrgb&w=1600',
+});
+productList.push({
+  name: 'Chopper',
+  price: 120,
+  image: 'https://images.pexels.com/photos/8936917/pexels-photo-8936917.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+});
+productList.push({
+  name: 'Chopper',
+  price: 7000,
+  image: 'https://images.pexels.com/photos/163789/sky-road-travel-trip-163789.jpeg?auto=compress&cs=tinysrgb&w=1600',
+});
+productList.push({
+  name: 'GP Bike',
+  price: 5500,
+  image: 'https://images.pexels.com/photos/1119796/pexels-photo-1119796.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+});
+productList.push({
+  name: 'Bike',
+  price: 120,
+  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productList.push({
+  name: 'Scoter',
+  price: 4200,
+  image: 'https://images.pexels.com/photos/159192/vespa-roller-motor-scooter-cult-159192.jpeg?auto=compress&cs=tinysrgb&w=1600',
+});
+productList.push({
+  name: 'Chopper',
+  price: 120,
+  image: 'https://images.pexels.com/photos/8936917/pexels-photo-8936917.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+});
+productList.push({
+  name: 'Chopper',
+  price: 7000,
+  image: 'https://images.pexels.com/photos/163789/sky-road-travel-trip-163789.jpeg?auto=compress&cs=tinysrgb&w=1600',
+});
+productList.push({
+  name: 'GP Bike',
+  price: 5500,
+  image: 'https://images.pexels.com/photos/1119796/pexels-photo-1119796.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+});
 
-/* agregamos elementos al array */
-productList.push({
-  name: 'Bike',
-  price: 120,
-  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-});
-productList.push({
-  name: 'Scoter',
-  price: 4200,
-  image: 'https://images.pexels.com/photos/159192/vespa-roller-motor-scooter-cult-159192.jpeg?auto=compress&cs=tinysrgb&w=1600',
-});
-productList.push({
-  name: 'Chopper',
-  price: 120,
-  image: 'https://images.pexels.com/photos/8936917/pexels-photo-8936917.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-});
-productList.push({
-  name: 'Chopper',
-  price: 7000,
-  image: 'https://images.pexels.com/photos/163789/sky-road-travel-trip-163789.jpeg?auto=compress&cs=tinysrgb&w=1600',
-});
-productList.push({
-  name: 'GP Bike',
-  price: 5500,
-  image: 'https://images.pexels.com/photos/1119796/pexels-photo-1119796.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-});
-productList.push({
-  name: 'Bike',
-  price: 120,
-  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-});
-productList.push({
-  name: 'Scoter',
-  price: 4200,
-  image: 'https://images.pexels.com/photos/159192/vespa-roller-motor-scooter-cult-159192.jpeg?auto=compress&cs=tinysrgb&w=1600',
-});
-productList.push({
-  name: 'Chopper',
-  price: 120,
-  image: 'https://images.pexels.com/photos/8936917/pexels-photo-8936917.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-});
-productList.push({
-  name: 'Chopper',
-  price: 7000,
-  image: 'https://images.pexels.com/photos/163789/sky-road-travel-trip-163789.jpeg?auto=compress&cs=tinysrgb&w=1600',
-});
-productList.push({
-  name: 'GP Bike',
-  price: 5500,
-  image: 'https://images.pexels.com/photos/1119796/pexels-photo-1119796.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-});
 function renderProducts(arr) {
   for (product of arr) {
-    /*captura de componenetes */
-    const productCard = document.createElement("div");
-    productCard.classList.add("product-card");
-    const productImg = document.createElement("img");
-    productImg.setAttribute("src", product.image);
-    const productInfo = document.createElement("div");
-    productInfo.classList.add("product-info");
-    const productInfoDiv = document.createElement("div");
-    const productPrice = document.createElement("p");
-    productPrice.innerText = "$" + product.price;
-    const productName = document.createElement("p");
+    const productCard = document.createElement('div');
+    productCard.classList.add('product-card');
+
+    // product= {name, price, image} -> product.image
+    const productImg = document.createElement('img');
+    productImg.setAttribute('src', product.image);
+    productImg.addEventListener('click', openProductDetailAside);
+
+    const productInfo = document.createElement('div');
+    productInfo.classList.add('product-info');
+
+    const productInfoDiv = document.createElement('div');
+
+    const productPrice = document.createElement('p');
+    productPrice.innerText = '$' + product.price;
+    const productName = document.createElement('p');
     productName.innerText = product.name;
 
-    /* maquetacion */
     productInfoDiv.appendChild(productPrice);
     productInfoDiv.appendChild(productName);
-    const productInfoFigure = document.createElement("figure");
-    const productImgCart = document.createElement("img");
-    productImgCart.setAttribute("src", "./icons/bt_add_to_cart.svg");
+
+    const productInfoFigure = document.createElement('figure');
+    const productImgCart = document.createElement('img');
+    productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+
     productInfoFigure.appendChild(productImgCart);
+
     productInfo.appendChild(productInfoDiv);
     productInfo.appendChild(productInfoFigure);
+
     productCard.appendChild(productImg);
     productCard.appendChild(productInfo);
+
     cardsContainer.appendChild(productCard);
   }
 }
+
 renderProducts(productList);
-
-
-
-
